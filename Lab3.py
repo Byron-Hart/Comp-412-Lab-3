3627739c8205df3b32096998fe54dfd0bc3823b0
Byron-Hart <byron.l.hart@gmail.com>
2024-12-04 18:45:49 -0600

    Early finish first test

--- Lab3.py
+++ Lab3.py
@@ -907,9 +907,25 @@ def schedule():
                         edge[1].children.remove(node)
                     if len(edge[1].children) == 0:
                         ready.add(edge[1])
-                        
         for node in toremove:
             active.remove(node)
+        
+        #Find early finishers
+        for node in active:
+            if node.latency == 5:
+                edgecounts = {}
+                for edge in node.edges:
+                    if not edgecounts[edge[1].num]:
+                        edgecounts[edge[1].num] = 1
+                    else:
+                        edgecounts[edge[1].num] += 1
+                for edge in node.edges:
+                    if node in edge[1].children and edgecounts[edge[1].num] == 1:
+                        edge[1].children.remove(node)
+                    if len(edge[1].children) == 0:
+                        ready.add(edge[1])
+
+                    
             
     return schedule
 
